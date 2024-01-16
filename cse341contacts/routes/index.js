8eecf391e9013122bbd264ad0ccb2c6497219d94
Use your GitHub username <Enter your email address>
2024-01-15 19:10:08 -0600

    added specific contacts route

--- cse341contacts/routes/index.js
+++ cse341contacts/routes/index.js
@@ -6,6 +6,9 @@ const contactController = require('../controllers/contactController');
 router.get('/', contactController.homePage);
 
 // GET contacts by id or favoriteColor
+router.get('/contacts', contactController.listContacts);
+
+// GET contacts by id or favoriteColor
 router.get('/contacts/:param?', contactController.listContacts);
 
 // POST a new contact
