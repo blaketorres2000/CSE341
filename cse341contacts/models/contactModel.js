3d5634e4526e243480007a9d25e83b609795dff2
Use your GitHub username <Enter your email address>
2024-01-22 08:40:07 -0600

    Fix email type in contactModel.js

--- cse341contacts/models/contactModel.js
+++ cse341contacts/models/contactModel.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const contactSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: email, required: true, unique: true },
   favoriteColor: { type: String, required: true },
   birthday: { type: Date, required: true },
 });
