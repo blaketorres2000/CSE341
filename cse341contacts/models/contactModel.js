3f291b903aa3c9398706953e0394d47d20c79386
Use your GitHub username <Enter your email address>
2024-01-22 08:49:28 -0600

    Update contactModel.js with required fields

--- cse341contacts/models/contactModel.js
+++ cse341contacts/models/contactModel.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
  * Connection Model and Schema
  ******************************************/
 const contactSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: email, required: true, unique: true },
-  favoriteColor: { type: String, required: true },
-  birthday: { type: Date, required: true },
+  firstName: { type: String, required: [true, 'First name is required'] },
+  lastName: { type: String, required: [true, 'Last name is required'] },
+  email: { type: String, required: [true, 'Email is required'], unique: true },
+  favoriteColor: { type: String, required: [true, 'Favorite color is required'] },
+  birthday: { type: Date, required: [true, 'Birthday is required'] },
 });
 
 const Contact = mongoose.model("Contact", contactSchema);
