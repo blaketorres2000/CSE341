ddbd2916cfb23e400ad099061d023f677ffd3de5
Use your GitHub username <Enter your email address>
2024-01-24 11:45:47 -0600

    Refactor API key handling in apiKeyMiddleware.js

--- cse341Project2/middleware/apiKeyMiddleware.js
+++ cse341Project2/middleware/apiKeyMiddleware.js
@@ -1,10 +1,6 @@
 const apiKeyMiddleware = (req, res, next) => {
     const expectedApiKey = process.env.API_KEY;
-    const providedApiKey = '5db70934345e409f96cb070e9495asdkjh54s534s2asd35as15a840ffa';
-
-    console.log('Incoming request headers:', req.headers); // Log all headers for debugging
-    console.log('Expected API key:', expectedApiKey); // Log the expected API key for debugging
-    console.log('Provided API key:', providedApiKey); // Log the provided API key for debugging
+    const providedApiKey = process.env.API_KEY;
     
     if (!providedApiKey || providedApiKey !== expectedApiKey) {
       console.log('Unauthorized. Invalid API key.');
